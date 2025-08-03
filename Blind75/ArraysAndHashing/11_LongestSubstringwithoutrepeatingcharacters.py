638a99551ccf0931b192d03ad3374ac96360d865
Rhea Pandita <98394329+RP-1106@users.noreply.github.com>
2025-08-03 12:49:01 +0530

    check out again

---
+++ Blind75/ArraysAndHashing/11_LongestSubstringwithoutrepeatingcharacters.py
@@ -0,0 +1,47 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        l=0
+        r=0
+
+        hash=set()
+        length=0
+
+        while r<len(s) and l<len(s):
+            if s[r] not in hash:
+                hash.add(s[r])
+                length=max(length,r-l+1)
+                r+=1
+
+            elif s[r] in hash:
+                while s[l]!=s[r] and l<r:
+                    hash.remove(s[l])
+                    l+=1
+
+                if s[l]==s[r]:
+                    hash.remove(s[l])
+                    l+=1
+
+                length = max(length,r-l+1)
+        return length
+  #------------OR--------------
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        l=0
+        r=0
+
+        hash=set()
+        length=0
+
+        while r<len(s) and l<len(s):
+            if s[r] not in hash:
+                hash.add(s[r])
+                length=max(length,r-l+1)
+                r+=1
+
+            else: 
+                hash.remove(s[l])
+                l+=1
+                length = max(length,r-l+1)
+                
+        return length
