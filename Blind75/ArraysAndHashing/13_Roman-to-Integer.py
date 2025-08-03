f3355ebfd2efa4c566f2f201d8eb7d2fd215b273
Rhea Pandita <98394329+RP-1106@users.noreply.github.com>
2025-08-03 22:40:40 +0530

    please try again

---
+++ Blind75/ArraysAndHashing/13_Roman-to-Integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        hash = {"I":1, "V":5, "X":10, "L":50,
+        "C":100, "D":500,"M":1000}
+
+        num=0
+        for i in range(len(s)):
+            if i+1<len(s) and hash[s[i]]<hash[s[i+1]]:
+                num-=hash[s[i]]
+            else:
+                num+=hash[s[i]]
+        return num
