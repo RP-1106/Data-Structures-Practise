cc96cad870101881dfbb99f32ed46e6970a382e6
Rhea Pandita <98394329+RP-1106@users.noreply.github.com>
2025-11-03 18:25:43 -0800

    Time: 53 ms (6.04%), Space: 12.5 MB (50.15%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -6,9 +6,17 @@ class Solution(object):
         """
         if n==1:
             return False
+        #INDEX0=>N=1
+        #index1=>n=2
+        OPT=[False for i in range(n+1)]
+        for i in range(2,n+1):
+            OPT[i]=False
+            for x in range(1,i):
+                if i%x==0:
+                    if OPT[i-x]==False:
+                        OPT[i]=True
+                        break
+        return OPT[n]
 
-        if n%2==0:
-            return True
-        else:
-            return False
+        
         
