b845be754355264c946c454e7ed942a180cc2496
Rhea Pandita <98394329+RP-1106@users.noreply.github.com>
2025-11-03 20:22:22 -0800

    Time: 0 ms (100%), Space: 12.4 MB (51.42%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.py
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.py
@@ -5,17 +5,22 @@ class Solution(object):
         :type word: str
         :rtype: int
         """
-        if word not in sequence:
-            return 0
-
         n = len(sequence)
         m = len(word)
-        OPT=0
-        
-        for i in range(1, n+1):
-            if (word*i) in sequence:
-                OPT+=1
+        maxk=0
+        OPT=[0 for i in range(n)]
+
+        #if len(word)=2, check if seq[0,1 (inclusive)]=word (last index=1)
+        #if len(word)=4, check if seq[0,1,2,3]=word (last index=3)
+        #if len(word)=m, check seq from 0 to m-1 (last index=m-1)
+        #start from the first possible index where 'word' can end 
+        for i in range(m-1,n):
+            if sequence[i-m+1: i+1]==word:
+                if i-m>=0:
+                    OPT[i] = OPT[i-m]+1
+                else:
+                    OPT[i]=1
+                maxk = max(maxk, OPT[i])
             else:
-                break
-        return OPT
-        
+                OPT[i]=0
+        return maxk
