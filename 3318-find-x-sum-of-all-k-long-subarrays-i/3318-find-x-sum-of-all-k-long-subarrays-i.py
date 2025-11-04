dfa6650abbf16d46190e73cbfa5336503100e877
Rhea Pandita <98394329+RP-1106@users.noreply.github.com>
2025-11-03 23:11:57 -0800

    Time: 47 ms (63.64%), Space: 12.4 MB (81.82%) - LeetHub

---
+++ 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def findXSum(self, nums, k, x):
+
+        output = []
+        n = len(nums)
+
+        def num_to_count(subarr):
+            num_to_count = {}
+            for i in subarr:
+                num_to_count[i] = 1 + num_to_count.get(i, 0)
+            return num_to_count
+
+        def count_to_num(num_to_count):
+            count_to_num = {}
+            for num, count in num_to_count.items():
+                count_to_num.setdefault(count, []).append(num)
+            return count_to_num
+        
+        for i in range(k - 1, n):
+            nlist = nums[i - k + 1 : i + 1]
+            n_t_c = num_to_count(nlist)
+            c_t_n = count_to_num(n_t_c)
+
+            sorted_list = [
+                (count, sorted(numbers, reverse=True))
+                for count, numbers in sorted(c_t_n.items(), reverse=True)
+            ]
+
+            result = []
+            remaining = x 
+
+            for count, numbers in sorted_list:
+                for num in numbers:
+                    result.extend([num] * count)
+                    remaining -= 1
+                    if remaining == 0:
+                        break
+                if remaining == 0:
+                    break
+
+            output.append(sum(result))
+        
+        return output
