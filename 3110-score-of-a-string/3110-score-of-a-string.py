f340956b9f8038e77555cf9a20d1c42e097f9f56
Rhea Pandita <98394329+RP-1106@users.noreply.github.com>
2026-02-03 22:19:08 -0800

    Time: 0 ms (100%), Space: 12.4 MB (86.24%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -0,0 +1,13 @@
+from math import *
+
+class Solution(object):
+    def scoreOfString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        score=0
+        n = len(s)
+        for i in range(1,n):
+            score += abs(ord(s[i])-ord(s[i-1]))
+        return score
