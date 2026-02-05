8f56968c41314847048646104c4a57088d898bcc
Rhea Pandita <98394329+RP-1106@users.noreply.github.com>
2026-02-04 21:16:45 -0800

    Time: 4 ms (18.62%), Space: 12.6 MB (51.93%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,26 +5,9 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if s=="" and t=="":
-            return True
-        if len(s)>0 and t=="":
-            return False
-        if len(t)>0 and s=="":
-            return True
-
-        m=len(s)
-        n=len(t)
-        OPT=[[False for j in range(n+1)] for i in range(m+1)]
-        for i in range(n+1):
-            OPT[0][i]=True
-
-        for i in range(1,m+1):
-            for j in range(1,n+1):
-                if s[i-1]==t[j-1]:
-                    OPT[i][j]=OPT[i-1][j-1]
-                else:
-                    OPT[i][j]=OPT[i][j-1]
-        return OPT[m][n]
-        
-
-
+        i,j = 0,0
+        while i<len(s) and j<len(t):
+            if s[i]==t[j]:
+                i+=1
+            j+=1
+        return i==len(s)
