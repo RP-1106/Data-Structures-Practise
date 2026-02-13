e18a8d4d4fb55fce00df96df5ee11bddb536f2a1
Rhea Pandita <98394329+RP-1106@users.noreply.github.com>
2026-02-12 22:34:54 -0800

    Time: 50 ms (9.5%), Space: 21.8 MB (7.13%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,29 +1,24 @@
-'''
-1. Find anagram of 1st word
-2. Add anagram to a dictionary d = {anagram:word}
-3. Find anagram of 2nd word. If its same as key in dictionary, add word to its value list. Else add anagram as a different key in the dictionary
-4. Return value pairs
-'''
-
-def compute_anagram(word):
-    h = {}
-    for i in range( len(word) ):
-        h[word[i]] = h.get(word[i],0) + 1
-    return h
+def comp_anagram( i):
+        count={}
+        for j in i:
+            count[j] = count.get(j,0)+1
+        return count
 
 class Solution(object):
     def groupAnagrams(self, strs):
-        d = dict()
-
-        for i in range( len(strs) ):
-            a = compute_anagram(strs[i])
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        hash = {}
+        for i in strs:
+            a = comp_anagram(i)
             key = tuple(sorted(a.items()))
 
-            if key not in d:
-                d[key] = [strs[i]]
+            if key not in hash:
+                hash[key] = [i]
             else:
-                d[key].append(strs[i])
+                hash[key].append(i)
+        return hash.values()
 
-        return d.values()
-            
-
+        
